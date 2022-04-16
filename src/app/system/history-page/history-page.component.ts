046b8327e4df91d441b9581390711f6be1439434
Olena Ryndina <olenaryndina1111@gmail.com>
2022-04-16 14:59:37 +0300

    create system module, add templates of pages: bill, history, planning, records

---
+++ src/app/system/history-page/history-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hm-history-page',
+  templateUrl: './history-page.component.html',
+  styleUrls: ['./history-page.component.less']
+})
+export class HistoryPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/system/system.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'hm-system',
+	templateUrl: './system.component.html',
+	styleUrls: ['./system.component.less']
+})
+
+export class SystemComponent {}

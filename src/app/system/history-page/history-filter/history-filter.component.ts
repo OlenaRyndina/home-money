4d0a13dfe096958e86dc1781c31670a839cb5283
Olena Ryndina <olenaryndina1111@gmail.com>
2022-04-27 15:50:31 +0300

    history-filter

--- src/app/system/history-page/history-filter/history-filter.component.ts
+++ src/app/system/history-page/history-filter/history-filter.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
+import {Category } from '../../shared/models/category.model';
 
 @Component({
-  selector: 'hm-history-filter',
-  templateUrl: './history-filter.component.html',
-  styleUrls: ['./history-filter.component.less']
+    selector: 'hm-history-filter',
+    templateUrl: './history-filter.component.html',
+    styleUrls: ['./history-filter.component.less']
 })
-export class HistoryFilterComponent implements OnInit {
+export class HistoryFilterComponent {
 
-  constructor() { }
+    @Output() onFilterCancel = new EventEmitter<any>();
+    @Output() onFilterApply = new EventEmitter<any>();
 
-  ngOnInit(): void {
-  }
+    @Input() categories: Category[] = [];
 
+    selectedPeriod = 'd';
+    selectedTypes = [];
+    selectedCategories = [];
+
+    timePeriods = [
+        {type: 'd', label: 'день'},
+        {type: 'w', label: 'неделя'},
+        {type: 'M', label: 'месяц'}
+    ]
+
+    types = [
+        {type: 'income', label: 'Доход'},
+        {type: 'outcome', label: 'Расход'}
+    ];
+
+    closeFilter() {
+        this.selectedTypes = [];
+        this.selectedCategories = [];
+        this.selectedPeriod = 'd';
+        this.onFilterCancel.emit();
+    }
+
+    private calculatedInputParams(field: string, checked: boolean, value: string) {
+        if(checked) {
+            this[field].indexOf(value) === -1 ? this[field].push(value) : null;
+        } else {
+            this[field] = this[field].filter(i => i !== value);
+        }      
+    }
+
+    handleChangeType(event) {
+        let checked = event.checked;
+        let value = event.value;
+        this.calculatedInputParams('selectedTypes', checked, value);      
+    }
+
+    handleChangeCategory(event) {
+        let checked = event.checked;
+        let value = event.value;
+        this.calculatedInputParams('selectedCategories', checked, value);
+    }
+
+    applyFilter() {
+        this.onFilterApply.emit({
+            types: this.selectedTypes,
+            categories: this.selectedCategories,
+            period: this.selectedPeriod
+        });
+    }
 }
--- src/app/system/history-page/history-page.component.ts
+++ src/app/system/history-page/history-page.component.ts
@@ -8,9 +8,9 @@ import { Category } from '../shared/models/category.model';
 import { HMEvent } from '../shared/models/event.model';
 
 @Component({
-  selector: 'hm-history-page',
-  templateUrl: './history-page.component.html',
-  styleUrls: ['./history-page.component.less']
+    selector: 'hm-history-page',
+    templateUrl: './history-page.component.html',
+    styleUrls: ['./history-page.component.less']
 })
 export class HistoryPageComponent implements OnInit, OnDestroy {
 
@@ -23,8 +23,11 @@ export class HistoryPageComponent implements OnInit, OnDestroy {
 
     categories: Category[] = [];
     events: HMEvent[] = [];
+    filteredEvents: HMEvent[] = [];
 
     chartData: any = [];
+
+    isFilterVisible = false;
 
     ngOnInit() {
         this.sub1 = combineLatest(
@@ -33,29 +36,72 @@ export class HistoryPageComponent implements OnInit, OnDestroy {
       ).subscribe((data: [Category[], HMEvent[]]) => {
             this.categories = data[0];
             this.events = data[1];
+            this.setOriginalEvents();
             this.calculateChartData();
 
             this.isLoaded = true;
       });
   }
 
-  calculateChartData(): void {
-      this.categories.forEach((cat) => {
-          const catEvent = this.events.filter((e) => e.category === cat.id && e.type === 'outcome');
-          this.chartData.push({
-              name: cat.name,
-              value: catEvent.reduce((total, e) => {
-                  total +=e.amount;
-                  return total;
-            }, 0)
-          })
-      })
+  private setOriginalEvents() {
+    this.filteredEvents = this.events.slice();
   }
 
-  ngOnDestroy() {
-    if(this.sub1) {
-      this.sub1.unsubscribe();
+    calculateChartData(): void {
+        this.chartData = [];
+        
+        this.categories.forEach((cat) => {
+            const catEvent = this.filteredEvents.filter((e) => e.category === cat.id && e.type === 'outcome');
+            this.chartData.push({
+                name: cat.name,
+                value: catEvent.reduce((total, e) => {
+                    total +=e.amount;
+                    return total;
+              }, 0)
+            })
+        })
     }
+
+    private toggleFilterVisibility(dir: boolean) {
+        this.isFilterVisible = dir;
+    }
+
+    openFilter() {
+        this.toggleFilterVisibility(true);
   }
+
+    onFilterApply(filterData) {
+        this.toggleFilterVisibility(false); 
+        this.setOriginalEvents()
+
+        const startPeriod = moment().startOf(filterData.period).startOf('d');
+        const endPeroid = moment().endOf(filterData.period).endOf('d');
+        
+        this.filteredEvents = this.filteredEvents
+            .filter((e) => {
+                return filterData.types.indexOf(e.type) !== -1;
+            })   
+            .filter((e) => {
+                return filterData.categories.indexOf(e.category.toString()) !== -1;
+            }) 
+            .filter((e) => {
+                const momentDate = moment(e.date, 'DD.MM.YYYY HH:mm:ss');
+                return momentDate.isBetween(startPeriod, endPeroid);
+            });
+
+        this.calculateChartData();
+    }
+
+    onFilterCancel() {
+        this.toggleFilterVisibility(false);
+        this.setOriginalEvents();
+        this.calculateChartData();
+    }
+
+    ngOnDestroy() {
+        if(this.sub1) {
+            this.sub1.unsubscribe();
+        }
+    }
 
 }
